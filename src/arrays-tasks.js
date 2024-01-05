828ea2a8bbf288a9ddae65a947834ce713b23408
Aleh Kuis <pestleralex@gmail.com>
2024-01-05 20:29:30 +0300

    feat: createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -325,8 +325,8 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((acc, [a, b]) => acc + a - b, 0);
 }
 
 /**
@@ -341,8 +341,12 @@ function calculateBalance(/* arr */) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.length === 0
+    ? []
+    : [arr.slice(0, chunkSize)].concat(
+        createChunks(arr.slice(chunkSize), chunkSize)
+      );
 }
 
 /**
